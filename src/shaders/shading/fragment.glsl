4ac25fff57cd6458f2b81db32b7084591ae22074
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-12-23 09:11:35 +0100

    add directionalLight

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,6 +1,19 @@
 uniform vec3 uColor;
 
+varying vec3 vNormal;
+
 #include ../includes/ambientLight.glsl
+
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition)
+{
+    vec3 lightDirection = normalize(lightPosition);
+
+    // Shading
+    float shading = dot(normal, lightDirection);
+    shading = max(0.0, shading);
+
+    return lightColor * lightIntensity * shading;
+}
 
 void main()
 {
@@ -9,6 +22,11 @@ void main()
     // Lights
     vec3 light = vec3(0.0);
     light += ambientLight(vec3(1.0), 0.03);
+    light += directionalLight(
+    vec3(0.1, 0.1, 1.0),
+    1.0,
+    vNormal,
+    vec3(0.0, 0.0, 3.0));
     color *= light;
 
     // Final color
