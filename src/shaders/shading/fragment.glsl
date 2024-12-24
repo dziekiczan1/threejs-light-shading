241399de91b991370ae6bce5a396f1914237a815
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-12-24 14:39:47 +0100

    add view direction

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -1,32 +1,42 @@
 uniform vec3 uColor;
 
 varying vec3 vNormal;
+varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
 
-vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition)
+vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection)
 {
     vec3 lightDirection = normalize(lightPosition);
+    vec3 lightReflection = reflect(-lightPosition, normal);
 
     // Shading
     float shading = dot(normal, lightDirection);
     shading = max(0.0, shading);
+
+    // Specular
+    float specular = - dot(lightReflection, viewDirection);
+    specular = max(0.0, specular);
+    specular = pow(specular, 20.0);
 
     return lightColor * lightIntensity * shading;
 }
 
 void main()
 {
+    vec3 normal = normalize(vNormal);
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
     vec3 color = uColor;
 
     // Lights
     vec3 light = vec3(0.0);
     light += ambientLight(vec3(1.0), 0.03);
     light += directionalLight(
-    vec3(0.1, 0.1, 1.0),
-    1.0,
-    vNormal,
-    vec3(0.0, 0.0, 3.0));
+    vec3(0.1, 0.1, 1.0), // Light color
+    1.0,                 // Light intensity
+    normal,             // Normal
+    vec3(0.0, 0.0, 3.0), // Light position
+    viewDirection);      // View direction
     color *= light;
 
     // Final color
