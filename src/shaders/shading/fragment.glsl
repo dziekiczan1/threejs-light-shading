b773a565c43877b98cbe5061d01a898a00dd3047
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-12-24 14:48:41 +0100

    seperate directionalLight

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -4,23 +4,7 @@ varying vec3 vNormal;
 varying vec3 vPosition;
 
 #include ../includes/ambientLight.glsl
-
-vec3 directionalLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection)
-{
-    vec3 lightDirection = normalize(lightPosition);
-    vec3 lightReflection = reflect(-lightPosition, normal);
-
-    // Shading
-    float shading = dot(normal, lightDirection);
-    shading = max(0.0, shading);
-
-    // Specular
-    float specular = - dot(lightReflection, viewDirection);
-    specular = max(0.0, specular);
-    specular = pow(specular, 20.0);
-
-    return lightColor * lightIntensity * shading;
-}
+#include ../includes/directionalLight.glsl
 
 void main()
 {
@@ -34,9 +18,10 @@ void main()
     light += directionalLight(
     vec3(0.1, 0.1, 1.0), // Light color
     1.0,                 // Light intensity
-    normal,             // Normal
+    normal,              // Normal
     vec3(0.0, 0.0, 3.0), // Light position
-    viewDirection);      // View direction
+    viewDirection,       // View direction
+    20.0);               // Specular power
     color *= light;
 
     // Final color
