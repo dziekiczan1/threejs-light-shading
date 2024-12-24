9577f104d3ff3f79c8ab4b1c088d7aa493b39007
dzekiczan1 <p.rzadkowolski@gmail.com>
2024-12-24 14:52:42 +0100

    create point light helper

--- src/shaders/shading/fragment.glsl
+++ src/shaders/shading/fragment.glsl
@@ -6,6 +6,23 @@ varying vec3 vPosition;
 #include ../includes/ambientLight.glsl
 #include ../includes/directionalLight.glsl
 
+vec3 pointLight(vec3 lightColor, float lightIntensity, vec3 normal, vec3 lightPosition, vec3 viewDirection, float specularPower)
+{
+    vec3 lightDirection = normalize(lightPosition);
+    vec3 lightReflection = reflect(-lightDirection, normal);
+
+    // Shading
+    float shading = dot(normal, lightDirection);
+    shading = max(0.0, shading);
+
+    // Specular
+    float specular = - dot(lightReflection, viewDirection);
+    specular = max(0.0, specular);
+    specular = pow(specular, specularPower);
+
+    return lightColor * lightIntensity * (shading + specular);
+}
+
 void main()
 {
     vec3 normal = normalize(vNormal);
@@ -14,14 +31,23 @@ void main()
 
     // Lights
     vec3 light = vec3(0.0);
-    light += ambientLight(vec3(1.0), 0.03);
-    light += directionalLight(
-    vec3(0.1, 0.1, 1.0), // Light color
-    1.0,                 // Light intensity
+//    light += ambientLight(vec3(1.0), 0.03);
+//    light += directionalLight(
+//    vec3(0.1, 0.1, 1.0), // Light color
+//    1.0,                 // Light intensity
+//    normal,              // Normal
+//    vec3(0.0, 0.0, 3.0), // Light position
+//    viewDirection,       // View direction
+//    20.0);               // Specular power
+
+    light += pointLight(
+    vec3(1.0, 0.1, 0.1), // Light color
+    1.0,                 // Light intensity,
     normal,              // Normal
-    vec3(0.0, 0.0, 3.0), // Light position
+    vec3(0.0, 2.5, 0.0), // Light position
     viewDirection,       // View direction
-    20.0);               // Specular power
+    20.0                 // Specular power
+    );
     color *= light;
 
     // Final color
